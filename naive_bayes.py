c3fb2b2154d678fe7faf967037021285ecaf408f
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2021-04-01 11:54:28 +0800

    Clean up LR model

--- naive_bayes.py
+++ naive_bayes.py
@@ -31,7 +31,7 @@ def generate_result(test, y_pred, filename):
 
 
 def run_model(path, ngram_range, partitioning_ratios):
-    print("Running model on " + path)
+    print("Running Naive Bayes model on " + path)
 
     train = pd.read_csv(path)
     raw_X_data = train['URL'].tolist()
@@ -42,22 +42,21 @@ def run_model(path, ngram_range, partitioning_ratios):
     last_train_idx = math.floor(partitioning_ratios[0]*num)
     last_validation_idx = last_train_idx + math.floor(partitioning_ratios[1]*num)
 
-    X_train = X_data[:last_train_idx]
-    y_train = y_data[:last_train_idx]
-
+    X_train = X_data[:last_validation_idx]
+    y_train = y_data[:last_validation_idx]
     model, count_vect = train_model(X_train, y_train, ngram_range)
 
     X_validation = X_data[last_train_idx+1:last_validation_idx]
     y_validation_answer = y_data[last_train_idx+1:last_validation_idx]
     y_validation_pred = predict(model, count_vect, X_validation)
-    score = f1_score(y_validation_answer, y_validation_pred, average='macro')
-    print(INDENTATION + 'Score on validation = {}'.format(score))
+    validation_score = f1_score(y_validation_answer, y_validation_pred, average='macro')
+    print(INDENTATION + 'Score on validation = {}'.format(validation_score))
 
     X_test = X_data[last_validation_idx+1:]
     y_test_answer = y_data[last_validation_idx+1:]
     y_test_pred = predict(model, count_vect, X_test)
-    score = f1_score(y_test_answer, y_test_pred, average='macro')
-    print(INDENTATION + 'Score on testing = {}'.format(score))
+    test_score = f1_score(y_test_answer, y_test_pred, average='macro')
+    print(INDENTATION + 'Score on testing = {}'.format(test_score))
 
 
 run_model("data/1_1_1_1_80000.csv", ngram_range=(1, 4), partitioning_ratios=(0.7, 0.2, 0.1))
