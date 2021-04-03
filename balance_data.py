c21795e29ea932c77150e3f9ffc4291634009496
anqichen9856 <chenanqi9856@gmail.com>
2021-04-04 01:49:15 +0800

    create balanced dataset from URL Classification_cleaned.csv

---
+++ balance_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+df = pd.read_csv('data/URL Classification_cleaned.csv', header=None)
+# TARGETED AT KIDS: Kids  
+# 2
+targeted_at_kids = df[df[1] == 'Kids']
+# print(len(targeted_at_kids))
+
+# SAFE FOR KIDS: Business, Computers, Health, Home, Reference, Science, Sports
+# 1
+safe_for_kids = df[(df[1] == 'Business') | (df[1] == 'Computers') | (df[1] == 'Health') | (df[1] == 'Home') 
+            | (df[1] == 'Reference') | (df[1] == 'Science') | (df[1] == 'Sports')]
+# print(len(safe_for_kids))
+
+# POTENTIALLY UNSAFE FOR KIDS: Arts, Shopping, Society, Recreation, News, Games 
+# -1
+potentially_unsafe_for_kids = df[(df[1] == 'Arts') | (df[1] == 'Shopping') | (df[1] == 'Society') 
+            | (df[1] == 'Recreation') | (df[1] == 'News') | (df[1] == 'Games')]
+# print(len(potentially_unsafe_for_kids))
+
+# UNSAFE FOR KIDS: Adult 
+# -2
+unsafe_for_kids = df[df[1] == 'Adult']
+# print(len(unsafe_for_kids))
+
+
+# 35123-35123-35123-35123 (1:1:1:1)
+targeted_at_kids_downsampled = targeted_at_kids.sample(n=35123, random_state=123)
+targeted_at_kids_downsampled.loc[:, 1] = 2
+safe_for_kids_downsampled = safe_for_kids.sample(n=35123, random_state=123)
+safe_for_kids_downsampled.loc[:, 1] = 1
+potentially_unsafe_for_kids_downsampled = potentially_unsafe_for_kids.sample(n=35123, random_state=123)
+potentially_unsafe_for_kids_downsampled.loc[:, 1] = -1
+unsafe_for_kids_downsampled = unsafe_for_kids.sample(frac=1)
+unsafe_for_kids_downsampled.loc[:, 1] = -2
+
+# print(len(targeted_at_kids_downsampled))
+# print(len(safe_for_kids_downsampled))
+# print(len(potentially_unsafe_for_kids_downsampled))
+# print(len(unsafe_for_kids_downsampled))
+
+total = pd.concat([targeted_at_kids_downsampled, safe_for_kids_downsampled, 
+    potentially_unsafe_for_kids_downsampled, unsafe_for_kids_downsampled], ignore_index=True)
+total_shuffled = total.sample(frac=1)
+total_shuffled.to_csv('balanced_data.csv', header=False, index=False)
